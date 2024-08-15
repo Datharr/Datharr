561465d8cf0d3a0c31eca2704f0f2ed580bac1db
Datharr <datharrgaming@gmail.com>
2024-08-15 15:53:13 +0200

    +Text

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-// index.js
+
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+
+
 let DATA = {
   name: 'Mathieu',
   date: new Date().toLocaleDateString('en-GB', {
@@ -18,18 +15,7 @@ let DATA = {
     timeZoneName: 'short',
     timeZone: 'Europe/Paris',
   }),
-  date2: new Date().toLocaleDateString('fr-FR', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Paris',
-  }),
   linkedin: "https://www.linkedin.com/in/mathieu-roche-efrei/",
-  cv_en: "https://github.com/Datharr/Datharr/blob/main/assets/CV-EN.pdf",
-  cv_fr: "https://github.com/Datharr/Datharr/blob/main/assets/CV-FR.pdf"
 };
 
 function generateReadMe() {
